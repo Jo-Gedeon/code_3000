eac1c19ca0369deddf35246249db3c9ac9ac9596
Jo <joseph.gedeon@uconn.edu>
2026-09-16 22:37:46 -0400

    lowered lr

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,7 +10,7 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.65,
+        learning_rate=0.01,
         n_estimators=800,
         max_depth=1,
         subsample=0.4,
